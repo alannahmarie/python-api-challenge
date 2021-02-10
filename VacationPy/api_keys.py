9eced5139b59050e61b7f7d31fafa4794ee68b85
Alannah Fleet <alannahmarie@Alannahs-MBP.fios-router.home>
2021-02-10 17:44:25 -0500

    All work complete

--- VacationPy/api_keys.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "8526d8192fc2775dd775065e20a72bcc"
-
-# Google API Key
-g_key = "AIzaSyDMR2zv7X09oJaBO0bRiWyY1s9CCmL3dNM"
--- WeatherPy/api_keys.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "8526d8192fc2775dd775065e20a72bcc"
-
-# Google API Key
-g_key = "AIzaSyDaFXGjcpNupaVm1MXnMDCHZYLz3PSe5-0"
